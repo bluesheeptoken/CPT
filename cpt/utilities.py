f80709e36b696449cb086616cbf524d21cf5783b
kimci86 <kimci86@hotmail.fr>
2018-12-13 00:47:44 +0100

    Put find_consequent's docstring where it is supposed to be

--- cpt/utilities.py
+++ cpt/utilities.py
@@ -1,13 +1,12 @@
 def sum_dictionnaries(dict1, dict2):
     return {k: dict1.get(k, 0) + dict2.get(k, 0) for k in set(dict1) | set(dict2)}
 
-
-"""
-The consequent of a sequence Y with respect to a sequence S 
-is the subsequence of Y starting after the last item in
-common with S until the end of Y
-"""
 def find_consequent(target_sequence, similar_sequence):
+    """
+    The consequent of a sequence Y with respect to a sequence S
+    is the subsequence of Y starting after the last item in
+    common with S until the end of Y
+    """
     for i in reversed(range(len(similar_sequence))):
         if similar_sequence[i] in target_sequence:
             return similar_sequence[i+1:]
