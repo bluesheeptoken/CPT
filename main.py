c803d22d218e4d4378206de1340189d05f799465
Bluesheeptoken <louis.fruleux1@gmail.com>
2018-12-13 14:41:49 +0100

    Removed useless main file

--- main.py
+++
@@ -1,9 +0,0 @@
-
-training_set = [['A', 'B', 'C'], ['A', 'B'], ['A', 'B', 'D'], ['B', 'C'], ['B', 'D', 'E']]
-
-cpt = Cpt(max_level=2)
-cpt.train(training_set)
-
-target_sequence = ['A', 'B', 'C']
-
-print(cpt.predict(target_sequence))
