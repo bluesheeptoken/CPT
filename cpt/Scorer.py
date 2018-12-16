411a30b02c3d60a0feff968c7982ef87a469a28c
Bluesheeptoken <louis.fruleux1@gmail.com>
2018-12-16 11:55:56 +0100

    Use heapq instead of sorted

--- cpt/Scorer.py
+++ cpt/Scorer.py
@@ -1,3 +1,5 @@
+import heapq
+
 class Scorer():
     def __init__(self, alphabet_length):
         self.alphabet_length = alphabet_length
@@ -12,8 +14,6 @@ class Scorer():
                 self.scoring[element] += 1
 
     def best_n_predictions(self, number_predictions):
-        return sorted(
-            range(self.alphabet_length),
-            key=lambda i: (self.scoring[i], i),
-            reverse=True
-        )[:number_predictions]
+        return heapq.nlargest(number_predictions,
+                              range(self.alphabet_length),
+                              key=lambda i: (self.scoring[i], i))
