3bd451b809abb48e73024b999faf58ff8edc3145
kimci86 <kimci86@hotmail.fr>
2018-12-19 23:42:39 +0100

    Refactor the PredictionTree class

--- cpt/PredictionTree.py
+++ cpt/PredictionTree.py
@@ -1,31 +1,26 @@
 class PredictionTree():
-    def __init__(self, item=None, parent=None):
-        self.item = item
+    def __init__(self, incoming_transition=None, parent=None):
         self.children = {}
+        self.incoming_transition = incoming_transition
         self.parent = parent
-
-    def has_child(self, element):
-        return element in self.children
-
-    def add_child(self, element):
-        self.children[element] = PredictionTree(element, self)
-        return True
 
     def get_child(self, element):
         return self.children.get(element, None)
 
-    def retrieve_path_from_root(self):
-        def loop(cursor, sequence):
-            if cursor.item is None:
-                return sequence
-            sequence.append(cursor.item)
-            return loop(cursor.parent, sequence)
-        return loop(self, [])[::-1]
+    def add_child(self, element):
+        return self.children.setdefault(element, PredictionTree(element, self))
+
+    def generate_path_to_root(self):
+        current = self
+        while current.incoming_transition is not None:
+            yield current.incoming_transition
+            current = current.parent
 
     def __repr__(self):
-        return "{{'item': {}, 'children': {}}}".format(self.item, list(self.children.values()))
+        return "{{'incoming_transition': {}, 'children': {}}}"\
+                .format(self.incoming_transition, list(self.children.values()))
 
     def __eq__(self, other):
         return isinstance(other, PredictionTree) \
-            and self.item == other.item \
+            and self.incoming_transition == other.incoming_transition \
             and self.children == other.children
