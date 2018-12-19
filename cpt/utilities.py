ac30f033c5469ab7aa52c74594269b5b6ddce7de
kimci86 <kimci86@hotmail.fr>
2018-12-20 00:13:59 +0100

    Rewrite the generate_consequent function

--- cpt/utilities.py
+++ cpt/utilities.py
@@ -1,10 +1,11 @@
-def find_consequent(target_sequence, similar_sequence):
+import itertools
+
+def generate_consequent(target_sequence, similar_sequence_end_node):
     """
     The consequent of a sequence Y with respect to a sequence S
     is the subsequence of Y starting after the last item in
     common with S until the end of Y
     """
-    for i in reversed(range(len(similar_sequence))):
-        if similar_sequence[i] in target_sequence:
-            return similar_sequence[i+1:]
-    return []
+    return itertools.takewhile(lambda i: i not in target_sequence,
+                               similar_sequence_end_node.generate_path_to_root())
+    # order is reversed, but it has no impact on the scoring
