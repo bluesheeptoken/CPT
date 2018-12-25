c35aabea0194fb2791c3046969d2b2389c1d12b8
Bluesheeptoken <louis.fruleux1@gmail.com>
2018-12-25 16:17:31 +0100

    Factorized with open

--- profiling.py
+++ profiling.py
@@ -7,11 +7,10 @@ from cpt.Cpt import Cpt
 def main():
     mode, data_path, output_path = sys.argv[1:]  # pylint: disable=unbalanced-tuple-unpacking
 
-    if data_path.endswith('.json'):
-        with open(data_path) as file:
+    with open(data_path) as file:
+        if data_path.endswith('.json'):
             data = list(json.load(file).values())
-    else:
-        with open(data_path) as file:
+        else:
             data = [[int(x) for x in l.rstrip().split()]
                     for l in file.readlines()]
 
