587a00325cc0344ba84eb95dc43372bcb90787e6
Bluesheeptoken <louis.fruleux1@gmail.com>
2018-12-28 23:42:07 +0100

    Added option to profile cython

--- profiling/profiling.py
+++ profiling/profiling.py
@@ -6,8 +6,6 @@ import os
 # Add cpt to python path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-from cpt.Cpt import Cpt  # pylint: disable=wrong-import-position
-
 
 def main():
     parser = argparse.ArgumentParser(description='Profile code for train or predict with cpt')
@@ -16,8 +14,18 @@ def main():
     parser.add_argument(dest='data_path', help='the data path file')
     parser.add_argument(dest='profile_path',
                         help='the output path file for the profile')
+    parser.add_argument('--cython', dest='cython', action='store_const',
+                        const=True, default=False,
+                        help='Set to true to use compiled cython cpt')
 
     args = parser.parse_args()
+
+    if args.cython:
+        from cptCython.Cpt import Cpt  # pylint: disable=wrong-import-position, no-name-in-module
+        print('Using Cython Cpt')
+    else:
+        from cpt.Cpt import Cpt  # pylint: disable=wrong-import-position
+        print('Using Python Cpt')
 
     if args.data_path.endswith('.json'):
         with open(args.data_path) as file:
