bf3ff8d46d1e55cc84edb3e23ac003704d37a266
Bluesheeptoken <louis.fruleux1@gmail.com>
2019-01-07 22:31:11 +0100

    Fixed PEP

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
 
+
 module = Extension('*', ['cpt/*.pyx'])
-# module.cython_c_in_temp = True 
 
 setup(ext_modules=cythonize(module, annotate=True, compiler_directives={'linetrace': True}))
