40014df00b0e6bbe17afc403b88d91ee82e4b2c6
Bluesheeptoken <louis.fruleux1@gmail.com>
2019-01-08 21:15:12 +0100

    Refactored scorer as an extension type

--- cpt/scorer.pyx
+++ cpt/scorer.pyx
@@ -1,18 +1,22 @@
 import heapq
 
 
-class Scorer():
-    def __init__(self, alphabet_length):
+cdef class Scorer:
+    def __cinit__(self, alphabet_length):
         self.alphabet_length = alphabet_length
         self.scoring = [0] * alphabet_length
 
     def predictable(self):
         return any(self.scoring)
 
-    def update(self, consequent_element):
+    def update(self, int consequent_element):
         self.scoring[consequent_element] += 1
 
     def best_n_predictions(self, number_predictions):
+
+        def get_score(int i):
+            return self.scoring[i]
+
         return heapq.nlargest(number_predictions,
                               range(self.alphabet_length),
-                              key=lambda i: self.scoring[i])
+                              key=get_score)
