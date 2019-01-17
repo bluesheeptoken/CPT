dab8db83957120c2de45300b8840a9fbc82d47c1
Bluesheeptoken <louis.fruleux1@gmail.com>
2019-01-17 20:55:55 +0100

    Added two cdef methods

--- cpt/scorer.pyx
+++ cpt/scorer.pyx
@@ -9,26 +9,23 @@ cdef class Scorer:
     def __cinit__(self, int alphabet_length):
         self.scoring = vector[int](alphabet_length)
 
+    cdef int get_score(self, int i):
+        return self.scoring.at(i)
+
     def predictable(self):
         return any(self.scoring)
 
-    def update(self, int consequent_element):
+    cpdef void update(self, int consequent_element):
         self.scoring[consequent_element] += 1
 
     def get_best_predictions(self, number_predictions):
 
-        def get_score(int i):
-            return self.scoring[i]
-
         return heapq.nlargest(number_predictions,
                               (x for x in range(self.scoring.size()) if self.scoring[x] != 0),
-                              key=get_score)
+                              key=lambda x: self.get_score(x))
 
     def get_best_prediction(self):
 
-        def get_score(int i):
-            return self.scoring[i]
-
         return max((x for x in range(self.scoring.size()) if self.scoring[x] != 0),
-                   key=get_score,
+                   key=lambda x: self.get_score(x),
                    default=NOT_AN_INDEX)
