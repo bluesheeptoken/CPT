876aa17e8dbba50a5fb3a58e3f306173175c9c6a
Bluesheeptoken <louis.fruleux1@gmail.com>
2019-02-27 11:00:53 +0100

    Added constructor from parameters and getters for prediction_tree

--- cpt/cpp_sources/PredictionTree.hpp
+++ cpt/cpp_sources/PredictionTree.hpp
@@ -10,12 +10,19 @@ class PredictionTree
 {
 public:
     PredictionTree();
+    PredictionTree(Node nextNode, std::vector<int> incoming, std::vector<int> parent, std::vector<std::map<int, Node>> children);
 
     Node addChild(Node parent, int transition);
 
     int getTransition(Node node) const { return m_incoming[node]; }
 
     Node getParent(Node node) const { return m_parent[node]; }
+
+    Node get_next_node() const { return m_nextNode; };
+
+    std::vector<int> get_incoming() const { return m_incoming; };
+    std::vector<int> get_parent() const { return m_parent; };
+    std::vector<std::map<int, Node>> get_children() const { return m_children; };
 
 private:
     Node m_nextNode;
--- cpt/prediction_tree.pxd
+++ cpt/prediction_tree.pxd
@@ -1,5 +1,7 @@
 # distutils: language = c++
 
+from libcpp.vector cimport vector
+from libcpp.map cimport map
 cdef extern from "cpp_sources/PredictionTree.cpp":
     pass
 
@@ -11,6 +13,12 @@ cdef enum:
 cdef extern from "cpp_sources/PredictionTree.hpp":
     cdef cppclass PredictionTree nogil:
         PredictionTree() except +
+        PredictionTree(Node, vector[int], vector[int], vector[map[int, Node]]) except +
         Node addChild(Node, int)
         int getTransition(Node)
         Node getParent(Node)
+
+        Node get_next_node()
+        vector[int] get_incoming()
+        vector[int] get_parent()
+        vector[map[int, Node]] get_children()
