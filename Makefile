ee5bdcfd9cf12d555f7ad2505b1ff7bee1a001bc
Bluesheeptoken <louis.fruleux1@gmail.com>
2019-04-05 14:09:17 +0200

    Updated Makefile for documentation
    Updated Makefile to make the documentation building successfully on
    readthedocs and in local.
    
    - Create a virutal env, install dependencies and the project
    - build doc
    - remove virtual env

--- Makefile
+++ Makefile
@@ -3,6 +3,7 @@ SRC_DIR=cpt
 TEST_DIR=tests
 REPORTS_DIR=reports
 COVERAGE_DIR=htmlcov
+VIRTUAL_ENV=cpt_documentation
 
 DEBUG=false
 
@@ -12,8 +13,6 @@ endif
 
 build: cpt/*.pyx
 	python3 setup.py build_ext --inplace ${trace_flag}
-	mkdir -p ${REPORTS_DIR}
-	find ${SRC_DIR}/*.html -exec mv {} ${REPORTS_DIR} \;
 
 clean:
 	rm -rf ${BUILD_DIR}/
@@ -23,6 +22,7 @@ clean:
 	rm -f ${SRC_DIR}/*.cpp
 	rm -rf ${REPORTS_DIR}
 	rm -rf ${COVERAGE_DIR}
+	rm -rf ${VIRTUAL_ENV}
 
 lint:
 	pylint ${TEST_DIR}
@@ -37,5 +37,11 @@ lint:
 test: build
 	pytest
 
-html: build
+html:
+	virtualenv ${VIRTUAL_ENV}
+	. ${VIRTUAL_ENV}/bin/activate
+	pip install -r doc/requirements.txt
+	python ./setup.py install --force
 	make -C doc clean html
+	. deactivate ${VIRTUAL_ENV}
+	rm -r ${VIRTUAL_ENV}
--- setup.py
+++ setup.py
@@ -2,19 +2,18 @@ import setuptools
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Build import cythonize
-import sys
+# import sys
 
 
 module = Extension('*',
         ['cpt/*.pyx'],
-        extra_compile_args=['-fopenmp'],
-        extra_link_args=['-fopenmp'])
+        extra_compile_args=['-fopenmp', '-std=c++11'],
+        extra_link_args=['-fopenmp', '-std=c++11'])
 
 setup(name="cpt",
-      ext_modules=cythonize(module,
-      annotate=True,
-      compiler_directives={
-                            'linetrace': True,
-                               'language_level': sys.version_info[0]
-                          }),
-      version="0.0.5")
+      ext_modules=cythonize(module, annotate=False),
+      # compiler_directives={
+      #                       'linetrace': True,
+      #                          'language_level': sys.version_info[0]
+      #                     }),
+      version="0.0.7")
