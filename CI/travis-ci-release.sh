7329d61a47b3658e147883bebf12e11c56a32aac
Louis Fruleux <louis.fruleux@PAR-MBP-LouisF.local>
2020-02-03 13:52:52 +0100

    Fixing wheels version on macos

--- CI/travis-ci-release.sh
+++ CI/travis-ci-release.sh
@@ -3,29 +3,38 @@ set -e
 
 umask 022
 chmod -R a+rX .
+if [ "$TRAVIS_OS_NAME" = "osx" ]; then
+    brew install llvm libomp;
+    export CPP=/usr/local/opt/llvm/bin/clang;
+    export PATH="/usr/local/opt/llvm/bin:$PATH";
+    REQUIRED_PYTHON_VERSION=$(python -c "import os; toxenv = os.environ['TOXENV']; print('.'.join(list(toxenv[2:])))");
+    brew install sashkab/python/python@$REQUIRED_PYTHON_VERSION
+    /usr/local/opt/python@$REQUIRED_PYTHON_VERSION/bin/python$REQUIRED_PYTHON_VERSION -mvenv venv
+    source venv/bin/activate
+fi
 
-pip3 install -U pip setuptools "wheel==0.31.1" twine
-python3 setup.py sdist
+pip install -U "pip < 19.1" -r CI/requirements.txt
+python setup.py sdist
 
 if [ -d build ]; then find build -name '*.py[co]' -delete; fi
-python3 setup.py build_ext
-python3 setup.py build --executable '/usr/bin/env python3'
-python3    -m compileall build
-python3 -O -m compileall build
-python3 setup.py bdist_wheel
+python setup.py build_ext
+python setup.py build --executable '/usr/bin/env python'
+python    -m compileall build
+python -O -m compileall build
+python setup.py bdist_wheel
 
-v=$(python3 -c "import sys; print(''.join(sys.version.split('.')[:2]))")
+v=$(python -c "import sys; print(''.join(sys.version.split('.')[:2]))")
 
 # if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-#     pip3 install -U auditwheel
+#     pip install -U auditwheel
 
 #     for f in dist/cpt-*-cp$v-*-linux_x86_64.whl; do
-#         python3 -m auditwheel repair -w dist/ "$f"
+#         python -m auditwheel repair -w dist/ "$f"
 #         rm -f "$f"
 #     done
 
 if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    pip3 install -U delocate
+    pip install -U delocate
 
     for f in dist/cpt-*-cp$v-*-macosx*.whl; do
         delocate-wheel -v "$f"
