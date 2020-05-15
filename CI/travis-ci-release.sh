47f09491d9375770cb9b5621a3071af41d0abc2e
louis <louis.fruleux@etudiant.mines-nantes.fr>
2020-05-15 18:08:28 +0200

    Update CI to include python3.8 and wheels for linux

--- CI/travis-ci-release.sh
+++ CI/travis-ci-release.sh
@@ -1,19 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-umask 022
-chmod -R a+rX .
-if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    brew install llvm libomp;
-    export CPP=/usr/local/opt/llvm/bin/clang;
-    export PATH="/usr/local/opt/llvm/bin:$PATH";
-    REQUIRED_PYTHON_VERSION=$(python -c "import os; toxenv = os.environ['TOXENV']; print('.'.join(list(toxenv[2:])))");
-    brew install sashkab/python/python@$REQUIRED_PYTHON_VERSION
-    /usr/local/opt/python@$REQUIRED_PYTHON_VERSION/bin/python$REQUIRED_PYTHON_VERSION -mvenv venv
-    source venv/bin/activate
+if [ "$TRAVIS_OS_NAME" = 'linux' ]; then
+    exit 0;
 fi
 
-pip install -U "pip < 19.1" -r CI/requirements.txt
+umask 022;
+chmod -R a+rX .;
+brew install llvm libomp;
+export CPP=/usr/local/opt/llvm/bin/clang;
+export PATH="/usr/local/opt/llvm/bin:$PATH";
+REQUIRED_PYTHON_VERSION=$(python -c "import os; toxenv = os.environ['TOXENV']; print('.'.join(list(toxenv[2:])))");
+brew install sashkab/python/python@$REQUIRED_PYTHON_VERSION
+/usr/local/opt/python@$REQUIRED_PYTHON_VERSION/bin/python$REQUIRED_PYTHON_VERSION -mvenv venv
+source venv/bin/activate
+
+pip install -U "pip < 19.2" "wheel==0.34.2" twine cython
 python setup.py sdist
 
 if [ -d build ]; then find build -name '*.py[co]' -delete; fi
@@ -25,22 +27,14 @@ python setup.py bdist_wheel
 
 v=$(python -c "import sys; print(''.join(sys.version.split('.')[:2]))")
 
-# if [ "$TRAVIS_OS_NAME" = "linux" ]; then
-#     pip install -U auditwheel
+pip install -U delocate
 
-#     for f in dist/cpt-*-cp$v-*-linux_x86_64.whl; do
-#         python -m auditwheel repair -w dist/ "$f"
-#         rm -f "$f"
-#     done
-
-if [ "$TRAVIS_OS_NAME" = "osx" ]; then
-    pip install -U delocate
-
-    for f in dist/cpt-*-cp$v-*-macosx*.whl; do
-        delocate-wheel -v "$f"
-    done
-fi
+for f in dist/cpt-*-cp$v-*-macosx*.whl; do
+    delocate-wheel -v "$f"
+done
 
 # TWINE_USERNAME / TWINE_PASSWORD / TWINE_REPOSITORY_URL
 # must be set in Travis settings.
+
+
 twine upload --skip-existing dist/* -u $TWINE_USERNAME -p $TWINE_PASSWORD -r $TWINE_REPOSITORY_URL
