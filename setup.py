26a59edcc9cf2c4a8b5c4aedecbf254476f023bc
Egor Shikov <45037864+ShikovEgor@users.noreply.github.com>
2024-10-01 13:58:29 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -53,5 +53,6 @@ setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=url,
+    install_requires=['Cython'],
     python_requires=">=3.8, <4",
 )
