4bb60cbe32e708f0d91e6f61ceb41cd4228c6ae5
Louis FRULEUX <louis.fruleux1@gmail.com>
2025-08-01 14:41:00 +0200

    Windows build on GitHub (#117)
    Now that Python3.8 is over and github supports windows runner for free for public repositories, let's simplify the build system and remove appveyor from the equation.
    
    Added builds for Windows amd and arm. For Python versions up to 3.14t

--- CI/upload_wheels.sh
+++
@@ -1,9 +0,0 @@
-upload_wheels() {
-  pwd
-  ls -l
-  python3 -m pip install twine
-  if [[ $TRAVIS_TAG ]]; then
-    python3 -m pip install twine
-    python3 -m twine upload ./wheelhouse/*.whl
-  fi
-}
